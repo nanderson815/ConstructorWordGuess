ec00ab143a9af0c4a720e8addbef8aa6a06c4feb
Noah A <nanderson815@gmail.com>
2019-02-04 20:33:43 -0500

    completed letter constructor

---
+++ Letter.js
@@ -0,0 +1,19 @@
+
+function Letter(){
+    this.char = char;
+    this.guessed = false;
+    this.guessedLetter = function(){
+        if (guessed){
+            return this.char;
+        } else {
+            return "_";
+        }
+    };
+    this.letterGuessed = function(C){
+        if (C === this.char){
+            this.guessed = ture;
+        }
+    };
+}
+
+module.exports = Letter;
