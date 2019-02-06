cc5c88fb5464f5e53758ca474e17b20a5e3e0fdc
Noah A <nanderson815@gmail.com>
2019-02-06 18:23:05 -0500

    added inquirer

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ var Word = require('./Word');
 
 var wordArray = ['baseball', 'football', 'basketball', 'bocce', 'tennis', 'hockey', 'skiing', 'swimming'];
 
-function chooseWord(){
-   var selection = wordArray[Math.floor(Math.random()*wordArray.length)];  
-   var activeWord = new Word(selection);
-   activeWord.wordArray();
-   console.log(activeWord);
+function chooseWord() {
+    var selection = wordArray[Math.floor(Math.random() * wordArray.length)];
+    var activeWord = new Word(selection);
+    activeWord.wordArray();
+    // console.log(activeWord);
 }
 
+function askQuestion(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Pick a letter!',
+                name: 'letter'
+            }
+
+        ]).then(function (answer) {
+            console.log(answer.letter);
+            return answer.letter;
+        });
+}
+
+
 chooseWord();
+askQuestion();
+
+    
