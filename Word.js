85ba8982b638c535603a7433eaf46947b917cc02
Noah A <nanderson815@gmail.com>
2019-02-06 17:39:35 -0500

    added the second word function

--- Word.js
+++ Word.js
@@ -1,24 +1,34 @@
 var Letter = require('./Letter');
 
-function Word(word){
+function Word(word) {
     this.letters = [];
-    this.wordString = function(){
-        for(i = 0; i < word.length; i++){
+    this.wordString = function () {
+        for (i = 0; i < word.length; i++) {
             this.letters.push((new Letter(word.charAt(i))));
         }
-        };
-    this.createString = function(){
+    };
+    this.createString = function () {
         var string = "";
-        for (var i in this.letters){
-        string = string.concat(this.letters[i].guessedLetter());
+        for (var i in this.letters) {
+            string = string.concat(this.letters[i].guessedLetter());
         }
         return string;
-    }
-    }
+    };
+    this.checkLetter = function (char) {
+        for (var i in this.letters) {
+            (this.letters[i].letterGuessed(char));
+        }
+    };
+}
 
 
 var hello = new Word("hello");
 
 hello.wordString();
-console.log(hello.createString());
+// console.log(hello.letters);
+// console.log(hello.createString());
+
+hello.checkLetter("h");
+
+
 
